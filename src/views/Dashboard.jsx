511e27200421b5ed1f78c65354173fde5228631c
pana-miguel <miguelangel.dominguez@unheval.pe>
2024-01-18 18:35:08 -0500

    Vista del componente Dashboard, solo para adminstradores

---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
+import { ConteinerDashboard } from '../components/ConteinerDashboard'
+
+export const Dashboard = () => {
+    return (
+        <>
+            <Header />
+            <ConteinerDashboard />
+            <Footer />
+        </>
+
+    )
+}
