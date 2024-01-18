c0f0c2efa091706a2494afeec822bbb6d2a739dd
pana-miguel <miguelangel.dominguez@unheval.pe>
2024-01-18 18:46:41 -0500

    Componente encargado de mostrar un boton clasico

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Button.css'
+
+export const Button = () => {
+    return (
+        <>
+            <button type='submit' className='buttonClasic'>
+                Ingresar
+            </button>
+        </>
+    )
+}
