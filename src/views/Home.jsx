4c81a568cab96ca047b378a22d028433569f7cda
pana-miguel <miguelangel.dominguez@unheval.pe>
2024-01-18 18:35:26 -0500

    Vista del componente Home

---
+++ src/views/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from '../components/Header'
+import { Footer } from '../components/Footer'
+import { ConteinerHome } from '../components/ConteinerHome'
+
+export const Home = () => {
+    return (
+        <>
+            <Header />
+            <ConteinerHome />
+            <Footer />
+        </>
+    )
+}
