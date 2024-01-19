d7191f98d6eaab3667dd75a2b95fd7a1bf7a8002
pana-miguel <miguelangel.dominguez@unheval.pe>
2024-01-18 19:04:21 -0500

    Componente encargado de mostrar el logo de la pagina

---
+++ src/components/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/Logo.css'
+
+export const Logo = () => {
+    return (
+        <>
+            <div className='Logo'>
+                Intra
+                <span className='logoEfect'>Wasi</span>
+            </div>
+        </>
+    )
+}
