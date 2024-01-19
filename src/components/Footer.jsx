9cf58d6b3c2106985883cc198020a0553a27c7fb
pana-miguel <miguelangel.dominguez@unheval.pe>
2024-01-18 19:03:29 -0500

    Componente encargado de mostrar el footer de la pagina

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+export const Footer = () => {
+    return (
+        <>
+            <div className="hero-links">
+                <a href="#" title=""><i className="bi bi-instagram"></i></a>
+                <a href="#" title=""><i className="bi bi-facebook"></i></a>
+                <a href="#" title=""><i className="bi bi-twitter"></i></a>
+                <a href="#" title=""><i className="bi bi-youtube"></i></a>
+            </div>
+        </>
+    )
+}
